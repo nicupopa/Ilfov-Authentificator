44145a082da45cb2eea3bebd8448b9d102034f2e
Ovi <ovinecula@gmail.com>
2023-11-18 22:02:54 +0200

    Prima modificare
    Pop-up tutorial #4

---
+++ popup/popup.js
@@ -0,0 +1,9 @@
+const IdElement = document.getElementById("UsernameLabel");
+const PassElement = document.getElementById("PasswordLabel");
+const URLElement = document.getElementById("URLLabel");
+
+const Submit = document.getElementById("SubmitButton");
+
+Submit.onclick = function () {
+  
+}
